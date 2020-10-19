c2f7a43b30a7cf861a8e34142873b631d146c955
Alla Kuzminova <alchik@gmail.com>
2020-10-19 23:58:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,7 +56,7 @@ class Song
     songname = filename.delete_suffix(".mp3")
     sngnm_arr = songname.split(" - ")
     puts "name" + sngnm_arr[1] +" artist "+sngnm_arr[0]
-    #newSong.name = sngnm_arr[0]
+    newSong.name = sngnm_arr[0]
     newSong.artist_name = sngnm_arr[1]
     newSong
   end
